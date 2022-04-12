4fc3bd0397d5e751624647dd6fa90a2184d0bc41
Nickolas Kenyeres <nickolas@knicklabs.com>
2022-04-11 22:44:56 -0400

    Add tailwind

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
+        <h1 className="title bg-red-500">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
